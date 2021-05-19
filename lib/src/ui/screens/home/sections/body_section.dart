c03db80a466c5ac686a2e9c0ff1683f65d5eb981
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2021-05-19 23:43:07 +0100

    Worked on home cards

--- lib/src/ui/screens/home/sections/body_section.dart
+++ lib/src/ui/screens/home/sections/body_section.dart
@@ -7,6 +7,12 @@ class BodySection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    var screenHeight = MediaQuery.of(context).size.height;
+
+    var cardWidth = screenWidth * 0.5;
+    var cardHeight = screenHeight / 3;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -14,13 +20,37 @@ class BodySection extends StatelessWidget {
         SizedBox(
           height: 16,
         ),
-        CardItem(
-          color: Colors.blueAccent,
-          iconData: Icons.note_add_outlined,
-          description: "All Tasks",
-          taskCount: 0,
-          onClick: onItemPressed,
-        ),
+        // CardItem(
+        //   color: Colors.blueAccent,
+        //   iconData: Icons.note_add_outlined,
+        //   description: "All Tasks",
+        //   taskCount: 0,
+        //   onClick: onItemPressed,
+        // ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: GridView.count(
+              // Create a grid with 2 columns. If you change the scrollDirection to
+              // horizontal, this produces 2 rows.
+              childAspectRatio: (cardWidth / cardHeight),
+              crossAxisCount: 2,
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+              children: List.generate(4, (index) {
+                return CardItem(
+                  // cardWidth: cardWidth,
+                  // cardHeight: cardHeight,
+                  color: Colors.blueAccent,
+                  iconData: Icons.note_add_outlined,
+                  description: "All Tasks",
+                  taskCount: 0,
+                  onClick: onItemPressed,
+                );
+              }),
+            ),
+          ),
+        )
       ],
     );
   }
